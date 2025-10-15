cf210ab53878200133aac09a1a8978c6af54faf2
Julia Mertz <info@juliamertz.dev>
2025-10-15 13:27:58 +0200

    chore: update to latest steiger
    clean

--- flake.nix
+++ flake.nix
@@ -2,81 +2,74 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
+    filter.url = "github:numtide/nix-filter";
+    steiger.url = "github:brainhivenl/steiger";
+    systems.url = "github:nix-systems/default";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    filter.url = "github:numtide/nix-filter";
-    steiger.url = "github:brainhivenl/steiger/feat/nix-force-host-platform";
   };
 
   outputs = {
     nixpkgs,
     crane,
-    flake-utils,
     rust-overlay,
     steiger,
     filter,
+    systems,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      localSystem: let
-        overlays = [(import rust-overlay) steiger.overlays.ociTools];
+  }: let
+    mkCraneLib = pkgs': (crane.mkLib pkgs').overrideToolchain (p: p.rust-bin.stable.latest.default);
+    overlays = [(import rust-overlay) steiger.overlays.ociTools];
+  in {
+    steigerImages = steiger.lib.eachCrossSystem (import systems) (localSystem: crossSystem: let
+      pkgs = import nixpkgs {
+        inherit overlays;
+        system = localSystem;
+      };
+      pkgsCross = import nixpkgs {
+        inherit overlays crossSystem localSystem;
+      };
 
-        pkgs = import nixpkgs {
-          inherit overlays;
-          system = localSystem;
-        };
+      craneLib = mkCraneLib pkgsCross;
 
-        crossSystem = let
-          steigerTarget = builtins.getEnv "STEIGER_TARGET_SYSTEM";
-        in
-          if builtins.stringLength steigerTarget == 0
-          then pkgs.system
-          else steigerTarget;
+      buildImage = package:
+        pkgs.ociTools.buildImage {
+          name = package.pname;
+          tag = "latest";
+          created = "now";
 
-        pkgsCross = import nixpkgs {
-          inherit overlays crossSystem localSystem;
-        };
-
-        mkCraneLib = pkgs': (crane.mkLib pkgs').overrideToolchain (p: p.rust-bin.stable.latest.default);
-        craneLib = mkCraneLib pkgs;
-        craneLibCross = mkCraneLib pkgsCross;
-
-        buildImage = package:
-          pkgs.ociTools.buildImage {
-            name = package.pname;
-            tag = "latest";
-            created = "now";
-
-            copyToRoot = pkgsCross.buildEnv {
-              name = "${package.pname}-sysroot";
-              paths = [
-                package
-                pkgs.dockerTools.caCertificates
-              ];
-              pathsToLink = [
-                "/bin"
-                "/etc"
-              ];
-            };
-
-            config.Cmd = ["/bin/${package.pname}"];
-            compressor = "none";
+          copyToRoot = pkgsCross.buildEnv {
+            name = "${package.pname}-sysroot";
+            paths = [
+              package
+              pkgs.dockerTools.caCertificates
+            ];
+            pathsToLink = [
+              "/bin"
+              "/etc"
+            ];
           };
 
-        headscale-operator = pkgsCross.callPackage ./package.nix {
-          craneLib = craneLibCross;
-          inherit filter;
-        };
-      in {
-        packages = {
-          default = headscale-operator;
-          image = buildImage headscale-operator;
+          config.Cmd = ["/bin/${package.pname}"];
+          compressor = "none";
         };
 
-        devShells.default = craneLib.devShell {
+      headscale-operator = pkgsCross.callPackage ./package.nix {
+        craneLib = craneLib;
+        inherit filter;
+      };
+    in {
+      headscale-operator = buildImage headscale-operator;
+    });
+
+    devShells = nixpkgs.lib.genAttrs (import systems) (
+      system: let
+        pkgs = import nixpkgs {inherit system overlays;};
+        craneLib = mkCraneLib pkgs;
+      in {
+        default = craneLib.devShell {
           packages = with pkgs; (let
             toolchain = pkgs.rust-bin.stable.latest.default.override {
               extensions = ["rust-src" "rustfmt"];
@@ -92,4 +85,5 @@
         };
       }
     );
+  };
 }
