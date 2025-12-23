2313d26be655de95f4909d5634fdc6e3503f239e
Julia Mertz <info@juliamertz.dev>
2025-12-23 22:44:27 +0100

    fix: selectively patch headscale resources

--- headscale-operator/src/admission/headscale.rs
+++ headscale-operator/src/admission/headscale.rs
@@ -6,18 +6,17 @@ use crate::{crds::Headscale, handlers::default_config_manager_image};
 pub async fn mutate(req: &AdmissionRequest<DynamicObject>) -> Result<AdmissionResponse, Error> {
     let res = AdmissionResponse::from(req);
     if Headscale::is(&req.kind) {
-        let patch = JsonPatch(vec![
-            PatchOperation::Test(TestOperation {
-                path: "/spec/configManager/image".parse()?,
-                value: "".into(),
-            }),
-            PatchOperation::Replace(ReplaceOperation {
+        let object = &req.object.clone().expect("headscale resource object");
+        let headscale: Headscale = parse_crd(object)?;
+
+        if headscale.spec.config_manager.image.is_empty() {
+            let patch = JsonPatch(vec![PatchOperation::Replace(ReplaceOperation {
                 path: "/spec/configManager/image".parse()?,
                 value: default_config_manager_image().into(),
-            }),
-        ]);
-        Ok(res.with_patch(patch)?)
-    } else {
-        Ok(res)
+            })]);
+            return Ok(res.with_patch(patch)?);
+        }
     }
+
+    Ok(res)
 }
--- headscale-operator/src/admission/mod.rs
+++ headscale-operator/src/admission/mod.rs
@@ -1,9 +1,10 @@
+pub use crate::Error;
 pub use json_patch::Patch as JsonPatch;
-pub use json_patch::{AddOperation, PatchOperation, ReplaceOperation, TestOperation};
+pub use json_patch::{AddOperation, PatchOperation, ReplaceOperation};
 pub use kube::api::{DynamicObject, GroupVersionKind};
 pub use kube::core::admission::{AdmissionRequest, AdmissionResponse};
 pub use kubus::admission;
-pub use crate::Error;
+use serde::de::DeserializeOwned;
 
 pub mod headscale;
 pub mod sidecar;
@@ -29,4 +30,9 @@ impl<K: k8s_openapi::Resource> ResourceGvkExt for K {
     fn is(kind: &GroupVersionKind) -> bool {
         kind.group == K::GROUP && kind.version == K::VERSION && kind.kind == K::KIND
     }
+}
+
+fn parse_crd<T: DeserializeOwned>(obj: &DynamicObject) -> Result<T, serde_json::Error> {
+    let value = serde_json::to_value(obj)?;
+    serde_json::from_value(value)
 }
